f538a2c99c09f93048222aeaf265a090f10facf0
Seyma Bayezit <163021404+seymabyzt@users.noreply.github.com>
2024-11-18 00:04:22 +0300

    Update

--- retro-tool-app/client/app/room/[roomId]/page.tsx
+++ retro-tool-app/client/app/room/[roomId]/page.tsx
@@ -12,7 +12,7 @@ import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
 import StepDescription from "@/app/components/Atoms/StepDescription";
 
-const socket: Socket = io("https://retrotool4server.onrender.com");
+const socket: Socket = io("https://retrotool4servercurrent.onrender.com");
 
 const Room = ({ params }: any) => {
   const roomID = params.roomId;
